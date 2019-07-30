160604b8cb7e9856da2c483fd6e61e621082e780
Anmol Narang <anmolnarang49@gmail.com>
2019-07-30 14:47:37 +0530

    Hello World first commit

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class helloworld {
+
+    public static void main(String[] args) {
+
+       for(int i=1;i<6;i++){        
+               System.out.print("HELLO WORLD\n");
+       }
+
+
+    }
+}
